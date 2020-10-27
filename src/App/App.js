df336e054f1a3b073ef3b94b7bc0c5d2f46d436f
TadashiLee <584638605@qq.com>
2020-10-27 21:42:13 +0800

    [wenchang.li]feat:random students order

--- src/App/App.js
+++ src/App/App.js
@@ -8,6 +8,8 @@ class App extends Component {
     super(props);
     this.state = {
       students: [],
+      showTable: false,
+      showGroup: [],
     };
   }
 
@@ -21,15 +23,36 @@ class App extends Component {
       .then((resp) => {
         this.setState({
           students: resp.data,
+          showGroup: resp.data,
         });
       })
       .catch(() => {});
   };
 
+  changeGroup = () => {
+    this.setState((prev) => ({
+      showTable: true,
+      showGroup: prev.showGroup.sort(() => {
+        return 0.5 - Math.random();
+      }),
+    }));
+  };
+
   render() {
     return (
       <div data-testid="app" className="App">
-        <div>Hello World</div>
+        {this.state.showTable ? (
+          <div>
+            <div>分组列表</div>
+            <Students students={this.state.showGroup} />
+          </div>
+        ) : (
+          ''
+        )}
+        <button type="button" onClick={this.changeGroup}>
+          分组学员
+        </button>
+        <div>学员列表</div>
         <Students students={this.state.students} />
       </div>
     );
